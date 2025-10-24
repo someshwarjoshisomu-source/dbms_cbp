87047c86a4f8e24c05e697a88a5b48209e616bdf
SomeshwarJoshi <24071a0532@vnrvjiet.in>
2025-10-24 17:49:03 +0530

    init commit

---
+++ project-tracker-frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import StudentDashboard from "./pages/StudentDashboard";
+import MentorDashboard from "./pages/MentorDashboard";
+import CompanyDashboard from "./pages/CompanyDashboard";
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/student" element={<StudentDashboard />} />
+                <Route path="/mentor" element={<MentorDashboard />} />
+                <Route path="/company" element={<CompanyDashboard />} />
+            </Routes>
+        </Router>
+    );
+}
